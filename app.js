6656c14b0adc63f4635fb57196c3b2bc35309085
AndreaFZP <00015617@uca.edu.sv>
2019-11-23 16:48:08 -0600

    Editado

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var casasRouter = require('./routes/casas');
 var registerRouter = require('./routes/register');
 var deleteRouter = require('./routes/delete');
+var updateRouter = require('./routes/update');
 
 mongoose.connect(process.env.MONGO_URI, {
   useCreateIndex: true,
@@ -48,6 +49,7 @@ app.use('/', indexRouter);
 app.use('/casas', casasRouter);
 app.use('/register', registerRouter);
 app.use('/delete', deleteRouter);
+app.use('/update', updateRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
